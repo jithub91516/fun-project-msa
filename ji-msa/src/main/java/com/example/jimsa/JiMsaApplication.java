9610092e40f14b14bfde678b1040cdec2ef24299
Ji <cyb91516@gmail.com>
2023-12-12 23:11:21 +0900

    initial commit

---
+++ ji-msa/src/main/java/com/example/jimsa/JiMsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jimsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiMsaApplication.class, args);
+    }
+
+}
---
+++ ji-msa/src/test/java/com/example/jimsa/JiMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jimsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
